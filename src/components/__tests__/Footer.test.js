9fb5dcf75d1f5212799886f81602e87756c8a205
Vladyslav-Markevych <vladen035@gmail.com>
2024-08-14 16:39:23 +0300

    sdasdas

--- src/components/__tests__/Footer.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { Footer } from "../Footer/Footer";
-
-describe("Footer", () => {
-  it("should render components", () => {
-    render(<Footer />);
-    expect(screen.getByText("My Footer")).toBeInTheDocument();
-  });
-});
